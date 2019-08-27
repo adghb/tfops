9bce9c5d64a751b5ab8220a1b77b26ddcf98a1ee
Arvind_Das <arvind.das@harbingergroup.com>
2019-08-27 11:59:11 +0530

    2nd commit

--- variables.tf
+++ variables.tf
@@ -4,17 +4,17 @@ variable "count" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "terraform"
+  default     = "AD-TEST-UBUNTU"
 }
 
 variable "instance_type" {
   description = "AWS instance type"
-  default     = "t3.small"
+  default     = "t2.micro"
 }
 
 variable "ami" {
   description = "Base AMI to launch the instances"
 
   # Bitnami NGINX AMI
-  default = "ami-021acbdb89706aa89"
+  default = "ami-03515d8a01c426ebc"
 }
